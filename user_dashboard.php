8ac0906ac0cff12f5e54606650ab0c2c39989b1d
abdeali88 <38809496+abdeali88@users.noreply.github.com>
2019-10-20 19:10:12 +0530

    Add files via upload

---
+++ user_dashboard.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+if(!isset($_SESSION['user_email'])){
+    $_SESSION['msg']="You must log in to view this page";
+    header('location:user_page.php');
+}
+
+if(isset($_SESSION['logout'])){
+    session_destroy();
+    unset($_SESSION['user_email']);
+    header("location:user_page.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/user_dashboard.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Acme|Berkshire+Swash|Lobster|PT+Sans+Narrow|Patua+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|Black+Ops+One" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css ">
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+<div class="navbar-color">
+        <nav class="navbar navbar-expand-lg navbar-light static-top" style="background: rgba(0,0,0,0.7);">
+            <div class="container">
+                <a class="navbar-brand navbar-img" href="home_page_1.html">
+                    <img src="img/hotel.png">&nbsp; Hostel Guru
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                    </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="home_page_1.html">HOME</a>
+                        </li>
+                        <li class="nav-item  active">
+                            <a class="nav-link" href="hostel_page.html">HOSTELS
+                                    <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="user_page.html">USER</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="manager_page.html">MANAGER</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+        <?php
+        if(isset($_SESSION['success'])) 
+        unset($_SESSION['success']);
+
+        if(isset($_SESSION['user_email'])) : ?>
+
+        <h3>Welcome<strong> <?php echo $_SESSION['user_name']; ?> </strong></h3>
+
+        <input class="btn btn-info" id="logout" type="button" value="Logout" name="logout_user">  
+        <script type="text/javascript">
+        document.getElementById("logout").onclick = function () {
+        location.href = "user_page.php?logout='1'";
+        };
+        </script>    
+
+        <?php endif ?>
+</body>
+</html>
