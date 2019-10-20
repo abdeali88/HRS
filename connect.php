f4a57e7e5e91fda142b314e5b85c9dcb7185d833
abdeali88 <38809496+abdeali88@users.noreply.github.com>
2019-10-20 19:09:29 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,26 +1,43 @@
 <?php
-$user_name = filter_input(INPUT_POST, 'user_name_signup'); 
-$user_email = filter_input(INPUT_POST, 'user_email_signup'); 
-$user_pass = filter_input(INPUT_POST, 'user_pass_signup'); 
-
+session_start();
 $host = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "hrs";
+$conn = mysqli_connect($host,$dbusername,$dbpassword,$dbname);
 
-$conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
+if(isset($_POST['register_user']))
+{
+
+$user_name = mysqli_real_escape_string($conn, $_POST['user_name_signup']); 
+$user_email = mysqli_real_escape_string($conn, $_POST['user_email_signup']); 
+$user_pass =  mysqli_real_escape_string($conn, $_POST['user_pass_signup']); 
+$errors=array();
+
 
 if(mysqli_connect_error()){
     die('Connect error ('.mysqli_connect_errno().')'.mysqli_connect_error());
 }
 
 else{
-    $insert_user = "INSERT INTO users(e_mail,name,password) values('$user_name','$user_email','$user_pass')";
-    if($conn->query($insert_user)){
-        echo "Insertion Sucessful";
+    $check_mul = "SELECT * FROM users WHERE e_mail='$user_email'";
+    $results = mysqli_query($conn,$check_mul);
+    if(mysqli_num_rows($results)==1)
+    {
+        array_push($errors,"Email is already taken");
+        ?> <script> alert("Email is already taken");</script> <?php 
     }
-    else{
-        echo "Error: ".$insert_user."<br>".$conn->error;
+    if(count($errors)==0)
+    {
+        $insert_user = "INSERT INTO users(e_mail,name,password) values('$user_email','$user_name','$user_pass')";
+        mysqli_query($conn,$insert_user);
+        $_SESSION['user_email'] = $user_email;
+        $_SESSION['user_name'] = $user_name;
+        $_SESSION['success'] =  "You are logged in";
+        header('location:user_dashboard.php');
     }
-    $conn->close();
 }
+}
+
+
+?>
