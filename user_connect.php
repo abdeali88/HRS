efe1ec5bfa786d235281ac4c1df3e689980ebff7
abdeali88 <38809496+abdeali88@users.noreply.github.com>
2019-10-24 16:27:14 +0530

    Update and rename connect.php to user_connect.php

--- user_connect.php
+++ user_connect.php
@@ -5,20 +5,15 @@ $dbusername = "root";
 $dbpassword = "";
 $dbname = "hrs";
 $conn = mysqli_connect($host,$dbusername,$dbpassword,$dbname);
-
 if(isset($_POST['register_user']))
 {
-
 $user_name = mysqli_real_escape_string($conn, $_POST['user_name_signup']); 
 $user_email = mysqli_real_escape_string($conn, $_POST['user_email_signup']); 
 $user_pass =  mysqli_real_escape_string($conn, $_POST['user_pass_signup']); 
 $errors=array();
-
-
 if(mysqli_connect_error()){
     die('Connect error ('.mysqli_connect_errno().')'.mysqli_connect_error());
 }
-
 else{
     $check_mul = "SELECT * FROM users WHERE e_mail='$user_email'";
     $results = mysqli_query($conn,$check_mul);
@@ -38,34 +33,28 @@ else{
     }
 }
 }
-
 if(isset($_POST['login_user'])){
     $user_email = mysqli_real_escape_string($conn, $_POST['user_email_login']); 
     $user_pass =  mysqli_real_escape_string($conn, $_POST['user_pass_login']); 
     $errors=array();
-
     if(mysqli_connect_error()){
         die('Connect error ('.mysqli_connect_errno().')'.mysqli_connect_error());
     }
     
     else{
         $auth = "SELECT * FROM users WHERE e_mail='$user_email'";
-
         $results_email = mysqli_query($conn,$auth);
         $rows = mysqli_fetch_assoc($results_email);
         $user_name = $rows['name'];
      
-
         if(empty($rows)){ 
             array_push($errors,"Invalid Email");
             ?><script> alert("Invalid Email!"); </script><?php
         }
-
         if($rows['e_mail']==$user_email && $rows['password']!=$user_pass){ 
             array_push($errors,"Invalid Password");
             ?><script> alert("Invalid Password!"); </script><?php
         }
-
         if(count($errors)==0){
             $_SESSION['user_email'] = $user_email;
             $_SESSION['user_name'] = $user_name;
